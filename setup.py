36f16a473b53f8fbf3293604f0b3e8eaefe377c6
AminHP <mdan.hagh@gmail.com>
2018-11-25 21:41:02 +0330

    Release v0.6.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='koala-serializer',
-    version='0.5.1',
+    version='0.6.0',
     description='A tool designed for creating serializable objects among different programming languages',
     long_description='',
     author='Koala',
